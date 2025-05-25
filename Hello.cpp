dc65ea01479c76b2a1af3e7e29a6c3c8c33f4ae7
Humbug!! <xmansussy72@gmail.com>
2025-05-25 00:28:52 +0000

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl; // Legit js prints Hello, World. 
     return 0;
 }
+// Idk why I made this. Let me live my life.
